b7c4ed53db79d4d2f16b21cc94f04503428e2cc6
Rex10202 <andresfelipegarciasosa35@gmail.com>
2023-06-06 19:50:01 -0500

    Version 2.0

---
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -0,0 +1,55 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*2️⃣ EJERCICIO 02 2️⃣*/
+
+function devolverString(string) {
+   return string;
+   // Debe retornar un string.
+   // Tu código:
+}
+
+// ⛔️ "X" e "Y" son números.
+
+function suma(x, y) {
+   var suma = x+y;
+   return suma;
+   // Retorna el resultado de su suma.
+   // Tu código:
+}
+
+function resta(x, y) {
+   var resta = x-y;
+   return resta;
+   // Retorna el resultado de la resta.
+   // Tu código:
+}
+
+function divide(x, y) {
+   var division = x/y;
+   return division;
+   // Retorna el resultado de su división.
+   // Tu código:
+}
+
+function multiplica(x, y) {
+   var multiplicacion = x*y;
+   return multiplicacion;
+   // Retorna el resultado de su multiplicación.
+   // Tu código:
+}
+
+function obtenerResto(x, y) {
+   var resto = x%y;
+   return resto;
+   // Obten el resto de la división de "x" entre "y".
+   // Tu código:
+}
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   devolverString,
+   suma,
+   resta,
+   divide,
+   multiplica,
+   obtenerResto,
+};
